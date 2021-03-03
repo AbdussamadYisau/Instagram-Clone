c11e540145882960e0052e3bcdccd493ce835a53
AbdussamadYisau <sammieyisau@gmail.com>
2021-03-03 01:39:59 +0100

    Working on UI

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container justify-content-center">
+    <div class="row">
+        <div class="col-3 pl-5 pr-5 pb-5">
+            <img src="https://scontent-los2-1.cdninstagram.com/v/t51.2885-19/s320x320/82565244_199952987815527_4846894752727040000_n.jpg?tp=1&_nc_ht=scontent-los2-1.cdninstagram.com&_nc_ohc=QNkQhTof5JYAX_PVvgE&oh=f48d3ecf822fabe9746b6ec2f936e473&oe=60673D5C" alt="Profile Picture" srcset="" class="rounded-circle" style="height: 150px; width: 150px;">
+        </div>
+        <div class="col-9">
+            <div>
+                <h1> sammieyisau</h1>
+            </div>
+
+            <div class="d-flex">
+                <div class="pr-4"><strong>30</strong> posts</div>
+                <div class="pr-4"><strong>421</strong> followers</div>
+                <div ><strong>530</strong> following</div>
+            </div>
+
+            <div class="pt-4 font-weight-bold">Abdussamad.</div>
+            <div>Writer.</div>
+            <div></div>
+        </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/c179.0.1081.1081a/s480x480/87699298_224177218739330_6877517616260521216_n.jpg?tp=1&_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=RoQ8J4pHNPMAX-58WlQ&oh=4c925e6b1cfd05d47a12eae16ad47c80&oe=6069B885" class="w-100" alt="MSP duties">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/s480x480/51915557_2170038599978733_4965154754275555845_n.jpg?tp=1&_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=JGuYyPVL5QQAX9jliZK&oh=a4b4acb2e4aef93d2a9475084325ce73&oe=60675179"class="w-100" alt="IT Defense">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.94.756.756a/s480x480/66382328_515822232488633_2589646706719388339_n.jpg?tp=1&_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=k9kKkNynlLUAX9iZKQb&oh=573078cefecad9da5a1f72039ef33547&oe=606695F6" class="w-100" alt="PAADC Hangout">
+        </div>
+    </div>
+</div>
+@endsection
---
+++ webpack.mix.js
@@ -0,0 +1,30 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
+
+mix.browserSync('http://127.0.0.1:8000');
+
+mix.browserSync({
+    proxy: 'http://127.0.0.1:8000',
+    files: [
+        './app/**/*',
+        './routes/**/*',
+        './public/css/*.css',
+        './public/js/*.js',
+        './resources/views/**/*.blade.php',
+        './resources/lang/**/*',
+    ]
+})
